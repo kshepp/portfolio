d62ec007cde662a61345229bd8ffcf02eddb8f89
kshepp <shepp@umich.edu>
2015-09-21 23:26:30 -0400

    Create betterhope.php

---
+++ betterhope.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>BetterHope</title>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<link href="css/custom.css" rel="stylesheet"> 
+
+
+</head>  
+
+<body>
+<?php include_once("analyticstracking.php") ?>
+<!--Start of Navbar area--> 
+
+<header>
+<?php 
+include ("nav.php");
+?>
+</header>
+<!--End of Navbar area--> 
+
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="0">
+
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+  </ol>
+ 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="img/Fall_Into_Consciousness.jpg" alt="Fall Into Consciousness Collage">
+    </div>
+    <div class="item">
+    	<a href="http://betterhope.is/fall-into-friendship/">
+      <img src="img/Fall_into_Friendship.jpg" alt="Fall Into Friendship Collage">
+  		</a>
+    </div>
+    <div class="item">
+    	<a href="http://betterhope.is/its-a-jungle-out-there/">
+      <img src="img/Its_a_Jungle_Out_There.jpg" alt="It's a Jungle Out There Collage">
+    	</a>
+    </div>
+    <div class="item">
+      <img src="img/Fall_Into_Providing.jpg" alt="Fall Into Providing Collage">
+    </div>
+    <div class="item">
+      <img src="img/Mans_Man.jpg" alt="Man's Day Out Collage">
+    </div>
+    <div class="item">
+      <img src="img/A_Harvest_of_Happiness.jpg" alt="A Harvest of Happiness Collage">
+    </div>
+  </div>
+ 
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div> <!-- Carousel -->
+
+<div class="description">
+<h2>What's BetterHope?</h2>
+
+<p>
+	BetterHope is a start-up that curates products made with dignity from the USA and 
+  around the world.
+	I create the product collages for the website, some of which you can scroll through above, 
+	as well as other
+	various technical related tasks. This includes tasks such as improving SEO, 
+  monitoring Google
+	Analytics, and helping make data driven decisions.
+<br />
+<br />
+  You can visit the BetterHope site <a href="http://www.betterhope.is"><b>here</b></a>.
+</p>
+
+<h2>Design Challenges</h2>
+<p>
+  "Ethical fashion" and "clothing made with dignity" as phrases are not a widely 
+  used by the American consumer base. By using these terms I'm refering to whether
+  the producers of the clothing receive a living wage.
+   As a designer and marketer, I work to connect
+  producers and consumers through an experience on the BetterHope site. BetterHope is
+   challenged to show that ethical clothing can be fashionable and affordable.
+</p>
+
+<h2>Projects</h2>
+
+<h3><em>Product Collages</em></h3>
+<p>
+  Part of my work at BetterHope includes creating product collages to highlight combination of
+  products on the site to create stylish outfits that fit any occasion. Often "ethical fashion"
+  can be seen as a "pity purchase" or have a "trinkety" feel, which is a 
+  steryeotype we try to fight through the collages.
+  <br />
+  <br />
+  Examples of some of the collages can be seen above as well as on the 
+  <a href="http://www.betterhope.is">BetterHope </a> site
+</p>
+
+<h3><em>Arc GIS: Connecting Producers to Consumers</em></h3>
+<p>
+  As part of the effort to connect Producers and Consumers and provide transparency on the
+  supply side process, I developed an Arc GIS map for BetterHope that highlights the brand
+  overview, the location of the brand's factories, and links to their products on
+  BetterHope.
+</p>
+<p>
+<div class="embed-responsive embed-responsive-16by9">
+<iframe width="100%" height="800px" src="http://www.arcgis.com/apps/MapJournal/?appid=65d88a4ff63543f7a4e03c1d55e665e3" frameborder="0" scrolling="no"></iframe>
+</div>
+</p>
+
+
+<h3><em>Google Analytics</em></h3>
+
+ <div class="text-center">
+<img class="Google" src="img/Google.png" alt="Picture of Kristen at Google">
+</div>
+<br />
+<p>
+  Another area I help out with is analyzing the organic and paid keywords in the 
+  Google Analytics account. This helped to ensure we were attracting quality traffic and gave me new ideas on the
+  types of collages to produce for each audience.
+  <a href="https://www.linkedin.com/profile/view?id=37426232">
+    Marianna</a>, the BetterHope founder,
+  and I both worked in Google AdWords previously and have a lot of experience in this area.
+</p>
+
+</div>
+
+   
+
+           
+
+
+ 
+<!--Start Footer area-->  
+<?php 
+include ("footer.php");
+?>
+<!--End of Footer Area-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+
+    
+</body>
+    
+</html>
