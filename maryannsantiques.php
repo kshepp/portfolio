2dc9a05859efde10ea47dfeefbc9367ef5e79ffc
kshepp <shepp@umich.edu>
2015-09-21 23:29:33 -0400

    Create maryannsantiques.php

---
+++ maryannsantiques.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Portfolio</title>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<link href="css/custom.css" rel="stylesheet"> 
+
+
+</head>  
+
+<body>
+<?php include_once("analyticstracking.php") ?>
+<!--Start of Navbar area--> 
+
+<header>
+<?php 
+include ("nav.php");
+?>
+</header>
+<!--End of Navbar area--> 
+
+<div class="container">
+<div class="text-center">
+<img class= "ma_logo" src="img/ma_logo.png" alt="Maryann's Antiques logo"/>
+</div>
+<div class="description">
+<h2>What's Maryann's Antiques?</h2>
+
+<p>
+	Maryann's Antiques is a small antiques business located in downtown Manistee, Michigan. The
+  owners, Jim and Maryann Sheppard, wanted to bring their business online to attract
+  a larger audience and increase their mobile presence. 
+<br />
+  You can visit the Maryann's Antiques site <a href="http://www.maryannsantiques.com"><b>here</b></a>.
+</p>
+
+<h2>The Evolution of Maryann's Antiques</h2>
+<p>
+More coming soon!
+</p>
+
+
+
+</div>
+</div>
+   
+
+           
+
+
+ 
+<!--Start Footer area-->  
+<?php 
+include ("footer.php");
+?>
+<!--End of Footer Area-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+
+    
+</body>
+    
+</html>
