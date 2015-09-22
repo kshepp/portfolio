9a2d7a7bfc819108a7d7828a876fae1323f410ce
kshepp <shepp@umich.edu>
2015-09-21 23:27:03 -0400

    Create broke.php

---
+++ broke.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>The Broke App</title>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+     
+<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+<link href="css/custom.css" rel="stylesheet"> 
+
+</head>  
+
+
+
+<!--Start of Navbar area--> 
+
+<header>
+<?php 
+include ("nav.php");
+?>
+</header>
+<!--End of Navbar area--> 
+
+<!--Start of Words-->
+
+
+
+<?php include_once("analyticstracking.php") ?>
+
+
+
+
+<div class="container">
+    <div class="row">
+        <div class="xs-col-12">
+<div class="text-center">
+<img class="piggy" src="img/broke_logo.jpg"/>
+
+<h2>What is the Broke App?</h2>
+
+<h4>
+The Broke App is a mobile, personal finance application for banks to reach and teach college students how 
+to build wealth for the future based on their current spending habits.
+<br />
+<br />
+For more information you can check out our <a href="http://www.thebrokeapp.com">website.</a>
+</h4>
+</div>
+<br />
+<br />
+<h3>Broke @ TechTown Detroit</h3>
+
+<p>
+For my internship in the summer of 2015, I participated in TechTown Detroit's DTX Launch program, 
+  which is a 10 week student accelerator program on customer discovery. During my time in TechTown and TechArb, 
+  where The Broke App incubates, I completed the following:
+</p>
+<ul>
+<li>Awarded the 'Get Set' award from TechTown and Bank of America for diligence in the customer discovery process</li>
+<li>Created a viable business canvas model</li>
+<li>Completed over 100 interviews with students, banks, financial literacy experts, investment firms, and 
+other entrepreneurs in the FinTech space</li>
+<li>Pitched weekly to members of the New Enterprise Forum of Michigan to receive feedback and refine 
+business canvas model</li>
+<li>Created and tested lo-fi and hi-fi prototypes (Sketch) to verify value provided as given in customer feedback</li>
+</ul>
+<br />
+<br />
+<br />
+
+<p>
+Our affinity wall of customer data. It's about 1/4 of the way complete in the picture:
+<img class="thumbnail" src="img/affinity_wall.jpg" alt="Affinity wall about 1/4 of the way done that we built at DTX" />
+<br />
+You can learn more about the DTX Launch Program here:
+<br />
+</p>
+<div class="text-center">
+  <div class="embed-responsive embed-responsive-16by9">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/fptOryf99IE" frameborder="0" allowfullscreen></iframe>
+  </div>
+</div>
+<br />
+<p>
+Here's the Twitter feed of the DTX Launch event the night I pitched and we won the 'Get Set' Award from TechTown:
+<img class="thumbnail" src="img/twitter_feed.jpg" />
+</p>
+
+<h3>Broke @ UMSI</h3>
+<p>
+  The Broke App began when our team met at the <a href="newyorktrek.php">New York Innovation Trek</a> and formed 
+  around a passion for financial literacy. Since New York we've been a lot of places pitching, iterating, and adapting 
+  the idea to feedback received. This video created by the <a href="https://www.si.umich.edu/">University of 
+  Michigan School of Information</a> shows our journey up through the Summer of 2015.
+  <br />
+  <br />
+  <div class="text-center">
+    <div class="embed-responsive embed-responsive-16by9">
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/XbWp87Y83K8" frameborder="0" allowfullscreen></iframe>
+  </div>
+  </div>
+</p>
+
+        </div>
+    </div>
+</div>
+
+
+ 
+<!--Start Footer area-->  
+<?php 
+include ("footer.php");
+?>
+<!--End of Footer Area-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+    
+</body>
+    
+</html>
